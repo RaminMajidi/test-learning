9f6244583393e46a3bad4dfee7a51a1bf6bd2ffc
RaminMajidi <raminmajididev01@gmail.com>
2026-07-28 16:55:44 +0330

    remove: all files

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { FormBuilderSuite } from '@flowcsolutions/react-form-builder';
-import '@flowcsolutions/react-form-builder/style.css';
-
-const  App = ()=> {
-  return <FormBuilderSuite />;
-}
-
-export default App;
